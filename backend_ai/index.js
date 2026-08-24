2a21f0d29011931de4d535ee73b0733c6bfb84be
Arpit Patel <arpitpatel00024@gmail.com>
2026-08-24 13:46:03 +0530

    Update CORS for deployed frontend

--- backend_ai/index.js
+++ backend_ai/index.js
@@ -7,14 +7,10 @@ require("./conn");
 
 app.use(express.json());
 
-app.use(
-    cors({
-        credentials: true,
-        origin: [
-            "http://localhost:5173"
-        ]
-    })
-);
+app.use(cors({
+    credentials: true,
+    origin: true
+}));
 
 const UserRoutes = require("./Routes/user");
 const ResumeRoutes = require("./Routes/resume");
